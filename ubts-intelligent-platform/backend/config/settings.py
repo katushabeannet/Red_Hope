7f5d6784251075a8267f97cc65562765e90c4978
okwii yakub <okwiiyakub@gmail.com>
2026-06-23 16:21:39 +0300

    removed cors headers error

--- ubts-intelligent-platform/backend/config/settings.py
+++ ubts-intelligent-platform/backend/config/settings.py
@@ -129,13 +129,11 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://redhope-frontend-era0.onrender.com/",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://redhope-frontend-era0.onrender.com/"
 ]
 
 SESSION_COOKIE_SAMESITE = "Lax"
